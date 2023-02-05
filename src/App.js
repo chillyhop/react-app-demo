993a18f28e611352e0b3fa5b4b0f69617bd79f05
chillyhop <apollomacabodbod11@gmail.com>
2023-02-05 10:39:12 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloads...
-
+          
+          React app demo
 
         </p>
         <a
